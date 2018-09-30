fc09aa79fde07736fdbf9d56ab78c9299af38d9f
GabrielChoi <gabychoi96@gmail.com>
2018-09-30 19:51:03 -0300

    m

---
+++ provincias.php
@@ -0,0 +1,7 @@
+<?php
+$provincias[
+  ['name'=>'Buenos Aires', 'dato'=>'buenosAires'],
+  ['name'=>'Cordoba', 'dato'=>'cordoba'],
+  ['name'=>'Misiones', 'dato'=>'misiones'],
+  ['name'=>'La Pampa', 'dato'=>'laPamap'],
+];
